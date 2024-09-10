e20bce28bff7e9c663edbe99789732261bdcfdc7
Aviroop Paul <aviroop.paul@think41.com>
2024-09-10 14:21:26 +0530

    US-1-FT-4--Create a job editing component that allows employers to modify existing job listings.

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import EmployerDashboard from './pages/EmployerDashboard';
+import EditJobPage from './pages/EditJobPage';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         </header>
         <Routes>
           <Route path="/employer-dashboard" element={<EmployerDashboard />} />
+          <Route path="/edit-job" element={<EditJobPage />} />
           {/* Other routes can be added here */}
         </Routes>
       </div>
